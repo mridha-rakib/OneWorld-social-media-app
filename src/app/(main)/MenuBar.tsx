10088ba745da47e654002e56c338d96e51317d7a
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-01-21 08:20:19 +0600

    [client]: Add menubar items to the client-side interface

--- src/app/(main)/MenuBar.tsx
+++ src/app/(main)/MenuBar.tsx
@@ -1,9 +1,64 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { Bookmark, Home, Bell, Mail } from "lucide-react";
+import Link from "next/link";
+import { validateRequest } from "@/auth";
 
 interface MenuBarProps {
   className?: string;
 }
 
-export default function MenuBar() {
-  return <div>MenuBar</div>;
+export default async function MenuBar({ className }: MenuBarProps) {
+  const { user } = await validateRequest();
+
+  if (!user) return null;
+
+  return (
+    <div className={className}>
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Home"
+        asChild
+      >
+        <Link href="/">
+          <Home />
+          <span className="hidden lg:inline">Home</span>
+        </Link>
+      </Button>
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Notifications"
+        asChild
+      >
+        <Link href="/notifications">
+          <Bell />
+          <span className="hidden lg:inline">Notifications</span>
+        </Link>
+      </Button>
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Messages"
+        asChild
+      >
+        <Link href="/messages">
+          <Mail />
+          <span className="hidden lg:inline">Messages</span>
+        </Link>
+      </Button>
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Bookmarks"
+        asChild
+      >
+        <Link href="/bookmarks">
+          <Bookmark />
+          <span className="hidden lg:inline">Bookmarks</span>
+        </Link>
+      </Button>
+    </div>
+  );
 }

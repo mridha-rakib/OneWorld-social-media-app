48f865affc8c0d01d97587efbb2acb5c8239408b
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-02-21 19:51:58 +0600

    [client}: complete user follow button functionality

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -4,7 +4,7 @@ import { useToast } from "@/hooks/use-toast";
 import useFollowerInfo from "@/hooks/useFollowerInfo";
 import kyInstance from "@/lib/ky";
 import { FollowerInfo } from "@/lib/types";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { QueryKey, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button } from "./ui/button";
 
 interface FollowButtonProps {
@@ -17,15 +17,40 @@ export default function FollowButton({
   initialState,
 }: FollowButtonProps) {
   const { toast } = useToast();
+
   const queryClient = useQueryClient();
 
   const { data } = useFollowerInfo(userId, initialState);
+
+  const queryKey: QueryKey = ["follower-info", userId];
 
   const { mutate } = useMutation({
     mutationFn: () =>
       data.isFollowedByUser
         ? kyInstance.delete(`/api/users/${userId}/followers`)
         : kyInstance.post(`/api/users/${userId}/followers`),
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey });
+
+      const previousState = queryClient.getQueryData<FollowerInfo>(queryKey);
+
+      queryClient.setQueryData<FollowerInfo>(queryKey, () => ({
+        followers:
+          (previousState?.followers || 0) +
+          (previousState?.isFollowedByUser ? -1 : 1),
+        isFollowedByUser: !previousState?.isFollowedByUser,
+      }));
+
+      return { previousState };
+    },
+    onError(error, variables, context) {
+      queryClient.setQueryData(queryKey, context?.previousState);
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Please try again.",
+      });
+    },
   });
 
   return (
@@ -33,7 +58,7 @@ export default function FollowButton({
       variant={data.isFollowedByUser ? "secondary" : "default"}
       onClick={() => mutate()}
     >
-      {data.isFollowedByUser ? "unfollow" : "follow"}
+      {data.isFollowedByUser ? "Unfollow" : "Follow"}
     </Button>
   );
 }

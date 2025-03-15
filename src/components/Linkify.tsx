56c7e1122267ed35b548bc9be07fe3b2bd5c741e
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-03-15 14:53:25 +0600

    [client]: user name and hastang linkify

--- src/components/Linkify.tsx
+++ src/components/Linkify.tsx
@@ -1,13 +1,57 @@
-import React from "react";
+import Link from "next/link";
+import { LinkIt, LinkItUrl } from "react-linkify-it";
+import UserLinkWithTooltip from "./UserLinkWithTooltip";
 
 interface LinkifyProps {
   children: React.ReactNode;
 }
 
 export default function Linkify({ children }: LinkifyProps) {
-  <LinkifyUrl>{children}</LinkifyUrl>;
+  return (
+    <LinkifyUsername>
+      <LinkifyHashtag>
+        <LinkifyUrl>{children}</LinkifyUrl>
+      </LinkifyHashtag>
+    </LinkifyUsername>
+  );
 }
 
 function LinkifyUrl({ children }: LinkifyProps) {
-  return <h3 className="text-primary hover:underline">Hello</h3>;
+  return (
+    <LinkItUrl className="text-primary hover:underline">{children}</LinkItUrl>
+  );
+}
+
+function LinkifyUsername({ children }: LinkifyProps) {
+  return (
+    <LinkIt
+      regex={/(@[a-zA-Z0-9_-]+)/}
+      component={(match, key) => (
+        <UserLinkWithTooltip key={key} username={match.slice(1)}>
+          {match}
+        </UserLinkWithTooltip>
+      )}
+    >
+      {children}
+    </LinkIt>
+  );
+}
+
+function LinkifyHashtag({ children }: LinkifyProps) {
+  return (
+    <LinkIt
+      regex={/(#[a-zA-Z0-9]+)/}
+      component={(match, key) => (
+        <Link
+          key={key}
+          href={`/hashtag/${match.slice(1)}`}
+          className="text-primary hover:underline"
+        >
+          {match}
+        </Link>
+      )}
+    >
+      {children}
+    </LinkIt>
+  );
 }
